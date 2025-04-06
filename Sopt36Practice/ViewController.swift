7ef352ddc6f3269eaa961aae7f2ae818f5838012
yeonee <leenayeon@sookmyung.ac.kr>
2025-04-07 02:21:58 +0900

    Initial Commit

---
+++ Sopt36Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt36Practice
+//
+//  Created by 이나연 on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
