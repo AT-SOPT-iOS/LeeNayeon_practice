ccdcd1025eb8359eea964f1fb0e32c12025ffe77
yeonee <leenayeon@sookmyung.ac.kr>
2025-04-08 00:04:11 +0900

    mod: welcome vc style 수정, backbutton 제거

--- Sopt36Practice/WelcomeViewController.swift
+++ Sopt36Practice/WelcomeViewController.swift
@@ -12,38 +12,39 @@ class WelcomeViewController: UIViewController {
     var id: String?
     
     private let logoImage : UIImageView = {
-        let image = UIImageView(frame: CGRect(x: 100, y: 50, width: 150, height: 150))
+        let image = UIImageView(frame: CGRect(x: 115, y: 87, width: 150, height: 150))
         image.image = UIImage(named: "welcomeVCimage")
         return image
     }()
     
     private let welcomeLabel : UILabel = {
-        let label = UILabel(frame: CGRect(x : 69, y : 200, width: 236, height: 80))
-//        label.text = "???님\n반가워요!"
+        let label = UILabel(frame: CGRect(x : 105, y : 295, width: 180, height: 60))
         label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
-        label.font = .boldSystemFont(ofSize: 25)
+        label.font = UIFont(name: "Pretendard-Bold", size: 18)
         return label
     }()
     
     private lazy var MainButton : UIButton = {
-        let button = UIButton(frame : CGRect(x: 21, y: 400, width: 322, height: 52))
+        let button = UIButton(frame : CGRect(x: 30, y: 420, width: 335, height: 57))
         button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
         button.setTitle("메인으로", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 18)
+        button.layer.cornerRadius = 3
 //        button.addTarget(self, action: #selector(loginButtonDidTapped), for: .touchUpInside)
         return button
     }()
     
     private lazy var BackLoginButton : UIButton = {
-        let button = UIButton(frame : CGRect(x: 21, y: 460, width: 322, height: 52))
+        let button = UIButton(frame : CGRect(x: 30, y: 490, width: 335, height: 57))
         button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
         button.setTitle("다시 로그인하기", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 18)
         button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
+        button.layer.cornerRadius = 3
         return button
     }()
 
